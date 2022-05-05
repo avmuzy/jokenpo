18921057c067e14810b8586dee4199de9511c156
avms <andrevmuzy@gmail.com>
2022-05-05 18:35:20 -0300

    def variaveis

--- JOKENPO.py
+++ JOKENPO.py
@@ -1,4 +1,5 @@
 from random import randint
+import time
 print('#' * 30)
 print('Bem vindo ao JOKENPO')
 print('#' * 30)
@@ -9,3 +10,8 @@ print('''Escolha uma das opcoes
 itens = ('Pedra', 'Papel', 'Tesoura')
 jogador = int(input('Qual a sua jogada?:'))
 comp = randint(0, 2)
+print('JO')
+time.sleep(1)
+print('KEN')
+time.sleep(1)
+print('PO!!!')
