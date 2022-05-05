ba2dbaaaaa14caa54bb67bfdfb9db86a0cd71db2
avms <andrevmuzy@gmail.com>
2022-05-05 18:43:57 -0300

    condicionais

--- JOKENPO.py
+++ JOKENPO.py
@@ -15,3 +15,16 @@ time.sleep(1)
 print('KEN')
 time.sleep(1)
 print('PO!!!')
+print('-=-'*11)
+print('Computador jogou {}'.format(itens[comp]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=-'*11)
+if comp == 0:
+    if jogador == 0:
+        print('EMPATOU')
+    elif jogador == 1:
+        print('Jogador VENCE!')
+    elif jogador == 2:
+        print('Computador Vence!')
+    else:
+        print('JOGADA INVALIDA')
